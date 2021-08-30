79a3264b4ba6e2a7bb86f5cbca0a2481ec43303c
ZhaooweiZhong <bobbie2zzw@outlook.com>
2021-08-30 20:49:19 +0800

    change

--- 12-goroutine/test1_goroutine.go
+++ 12-goroutine/test1_goroutine.go
@@ -3,16 +3,26 @@ import(
 	"fmt"
 	"time"
 )
+
+//子goroutine
 func newTask(){
 	i:=0
 	for{
 		i++
-		fmt.Printlf("new Goroutine :i = %d\n",i)
+		fmt.Printf("new Goroutine :i = %d\n",i)
 		time.Sleep(1 * time.Second)
 	}
 	
 }
-
+//主goroutine
 func main(){
+	//创建一个go程去执行newTask()流程
 	go newTask()
+
+	i:=0
+	for{
+		i++
+		fmt.Printf("main Goroutine :i = %d\n",i)
+		time.Sleep(1 * time.Second)
+	}
 }
